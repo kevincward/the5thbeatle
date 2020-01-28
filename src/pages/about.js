d46fa0b9c4e335a99af379e5666d27aadfab83aa
igor1156 <igorpadushinfx@outlook.com>
2020-01-28 23:18:52 +0200

    finish task5

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,39 +6,57 @@ import Layout from '../layouts'
 
 const Container = styled.div`
     display: flex;
-    padding: 150px 50px;
-    flex-direction: column;    
-    @media (max-width: 768px) {        
-    }
-`
-
-const Title = styled.div`
-    width: 565px;
-    font-family: Inter;
-    font-size: 28px;
-    font-weight: bold;
-    font-stretch: normal;
-    font-style: normal;
-    line-height: 1.2;
-    letter-spacing: normal;
-    color: #000000;
+    padding: 160px 30px 80px;
+    flex-direction: row;
     @media (max-width: 768px) {
-        width: 100%;
-        font-size: 32px;
+      flex-direction: column;
+      padding: 60px 15px 44px;
     }
 `
 
 const Text = styled.div`
-    font-family: Inter;
-    font-size: 14px;
-    font-weight: 500;
-    font-stretch: normal;
-    font-style: normal;
-    line-height: 1.5;
-    letter-spacing: 0.2px;
-    color: rgba(0, 0, 0, 0.7);
+  font-family: Inter;
+  font-size: 20px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.6;
+  letter-spacing: normal;
+  color: #333333;
 `
 
+const Img = styled.img`
+  width: 35%;
+  height: auto;
+  @media (max-width: 768px) {
+    width: 100%;
+    margin: 32px 0;
+  }
+`
+
+const Content = styled.div`
+  width: 60%;
+  margin-left: 80px;
+  @media (max-width: 768px) {
+    width: 100%;
+    margin-left: 0px;
+  }
+`
+
+const HeadLine = styled.div`
+  font-family: Inter;
+  font-size: 24px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.33;
+  letter-spacing: normal;
+  color: #333333;
+  margin-bottom: 44px;
+  @media (max-width: 768px) {
+    margin-bottom: 0px;
+  }
+`
 
 const About = () => {    
 
@@ -46,14 +64,24 @@ const About = () => {
   return (
     <Layout>
       <Container>
-        <Title>
-            I am the fifth Beatle. <br /><br />
-            While my band is on hiatus, I look for problems and design solutions.
-        </Title>
-        <Text>
-            I am the fifth Beatle. <br /><br />
-            While my band is on hiatus, I look for problems and design solutions.
-         </Text>
+        {window.matchMedia('(max-width: 768px)').matches ? 
+         <HeadLine>Headline</HeadLine>
+        : null}
+        <Img src={Me} />
+        <Content>
+          {!window.matchMedia('(max-width: 768px)').matches ? 
+            <HeadLine>Headline</HeadLine>
+          : null}
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+            ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+             in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+             Excepteur sint occaecat cupidatat non proident, 
+             sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </Text>
+        </Content>        
       </Container>
     </Layout>
   )
