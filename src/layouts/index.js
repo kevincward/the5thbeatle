1f879ad0c95a3b5a920c85b7dac8447613f3c1c8
kevincward <kevin@tcheck.me>
2020-02-07 08:42:44 -0800

    Metadata code

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,6 +8,14 @@ import Footer from '../components/footer';
 
 const Layout = ({ children, data }) => (
   <div>
+    {/*<Helmet
+      title={data.site.siteMetadata.title}
+      meta={[
+        {name: 'description', content:data.site.siteMetadata.description },
+        {name: 'keywords', content: 
+          data.site.siteMetadata.keywords },
+      ]}
+    />*/}
     <Header />
     {children}
       <Footer data={data}>
@@ -23,6 +31,21 @@ Layout.propTypes = {
   children: PropTypes.func,
 }
 
+
+
 export default Layout
+
+export const query = graphql`
+query SiteTitleQuery {
+  site {
+    siteMetadata {
+      title
+      description
+      keywords
+    }
+  }
+}
+`
+  
 
 
